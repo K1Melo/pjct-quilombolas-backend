b62d3b98e3088084f2f7bfa93305a6f3489bcfd2
Kauã Fernandes <kauafernandes.smelo@gmail.com>
2023-11-01 00:40:09 -0300

    Acces-Controls

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose');
 
 const app = express()
-app.use(express.json())
+app.use(express.static('public'))
+app.use(express.json({limit: '100kb'}))
+
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 const port = 3000
 
 const Message = mongoose.model('UserMessage', { 
@@ -15,7 +24,7 @@ app.get('/', async (req, res) => {
     return res.send(messages)
 })
 
-app.post('/', async (req, res) => {
+app.post('/', async (req, res) => {    
     const message = new Message({
         name: req.body.name,
         feedback: req.body.feedback,
